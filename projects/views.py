d7b9c8ea3416037d0528af368450a24eac6709bd
Jaz-B <44511160+Jaz-B@users.noreply.github.com>
2021-06-09 19:14:54 -0300

    add views: index and detail

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from projects.models import Project
+
+def project_index(request):
+    projects = Project.objects.all() #a query to retrieve all projects
+    # context :a dictionary used to send information to our template
+    context = {                    
+        'projects': projects
+    }
+    return render(request, 'project_index.html', context)
