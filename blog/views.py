37353de5ab3bc582c875482c354c780ccbaada20
Jaz-B <44511160+Jaz-B@users.noreply.github.com>
2021-07-01 13:28:16 -0300

    add blog view functions

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from blog.models import Post, Comment
 
-# Create your views here.
+def blog_index(request):
+    # ordered with the most recent post first.The minus sign tells Django to start with the largest value rather than the smallest. 
+    post = Post.objects.all().order_by('-created_on')
+
+    context = {
+        'posts': posts
+    }
+
+    return render(request, 'blog_detail.html', context)
+
+# Index posts from specific category chosen by the user
+def blog_category(request, category):
+    post = Post.objects.filter(categories__name__contains=category
+    ).order_by('-created_on')
+    
+    context = {
+        "category": category,
+        "posts": posts
+    }
+    
+    return render(request, "blog_category.html", context)
+
+def blog_detail(request,pk):
+    project = Blog.objects.get(pk=pk)
+    comments = Comment.objects.filter(post=post)
+    context = {
+        "post": post,
+        "comments": comments,
+    }
+
+    return render(request, "blog_detail.html", context)
