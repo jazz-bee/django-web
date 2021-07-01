181d9ca93e1759ce4df0a136f50489a88f5638de
Jaz-B <44511160+Jaz-B@users.noreply.github.com>
2021-07-01 12:50:48 -0300

    django admin: register models

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from blog.models import Post, Category
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+# we could customize what is shown on the admin pages but for now, default configuration is enough.
+
+# django admin console: localhost:8000/admin 
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
