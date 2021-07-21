9f506d2a181adf172a2bc24b36dd1b85928b5e3f
Jaz-B <44511160+Jaz-B@users.noreply.github.com>
2021-07-21 16:13:45 -0300

    code format

--- projects/views.py
+++ projects/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render
 
 from projects.models import Project
 
+
 def project_index(request):
-    projects = Project.objects.all() # a Django ORM query to retrieve all projects
+    projects = Project.objects.all()  # a Django ORM query to retrieve all projects
     # context:a dictionary used to send information to our template
-    context = {                    
+    context = {
         'projects': projects
     }
     return render(request, 'project_index.html', context)
+
 
 def project_detail(request, pk):
     # retrieves the project with primary key 'pk', equal to the one in the function argument
